e67eba83d278e9c271cd28e4ad3b4189f4b918b0
miho <paglinawan0520@gmail.com>
2024-03-11 20:52:51 +0900

    Learn: rename and add column

--- commands.sql
+++ commands.sql
@@ -1,12 +1,22 @@
 -- table
-CREATE table if not exists posts(
+
+-- delete table
+drop table if exists posts;
+
+CREATE table posts(
   id integer primary key,
   title text,
   body text
 );
 
+-- rename table
+alter table posts rename to articles;
+
 -- list tables
 .tables
+
+-- add column
+alter table articles add column email text;
 
 -- table structure
 .schema
