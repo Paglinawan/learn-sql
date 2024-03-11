fa3a3ca45e51783b1c1f6151ddb1bed241f59f3d
miho <paglinawan0520@gmail.com>
2024-03-11 21:16:54 +0900

    Learn: select syntax

--- commands.sql
+++ commands.sql
@@ -1,15 +1,30 @@
--- table
-
--- delete table
 drop table if exists users;
-
-CREATE table users(
+CREATE table users (
   id integer primary key,
-  name text not null,
-  score integer default 40 check (score >= 0),
-  email text unique
+  name text,
+  score integer
 );
 
+insert into users (name, score) values ('Miho', 65);
+insert into users (name, score) values ('Judith', 95);
+insert into users (name, score) values ('Jolly', 72);
+insert into users (name, score) values ('Ysha', 54);
+insert into users (name, score) values ('Ember', 40);
 
--- show all records
+-- select * from users;
+
+.headers on
+-- select id, name from users;
+-- select id, name as user_name from users;
+
+-- .mode line
+-- select * from users;
+
+.mode csv
+select * from users;
+
+.mode html
+select * from users;
+
+.mode column
 select * from users;
